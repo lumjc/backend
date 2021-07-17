19d1755dfd9b491b5cede2f24f5a20268bdfaa8b
JunChi <lumjc12@gmail.com>
2021-07-17 12:44:46 +0800

    fixed cors and gitignored node_modules

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const forumRoutes  = require("./routes/forum");
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/products")
 const bodyParser = require('body-parser')
+const cors = require("cors")
 const mongoose = require ('mongoose');
 const app = express()
 const port = 3000
@@ -16,35 +17,14 @@ app.get('/', (req, res) => {
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use('/uploads',express.static('uploads'))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({limit: '20mb' , extended:true}))
-app.use((req,res) =>{
-  res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Headers','*')
-  if(req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT','POST','PATCH','DELETE','GET')
-    return res.status(200).json({});
-  }
-})
+app.use(cors({origin: '*'}))
 
 app.use(forumRoutes)
 app.use(userRoutes)
 app.use(productRoutes)
 
-app.use((req,res,next)=>{
-  const error = new Error('Not found')
-  error.status = 404
-  next(error)
-})
-
-app.use((error, req, res, next)=> {
-  res.status(err.status || 500 )
-  res.json({
-    error : {
-      message: error.message
-    }
-  })
-})
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true , useUnifiedTopology: true })
 console.log(process.env.DATABASE_URL)
